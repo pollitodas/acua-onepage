555bf358b8777d9d8992c1bc9653437ab7c23364
pollitodas <damianmillan19@gmail.com>
2017-06-29 22:36:28 -0300

    test

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
+
+
+
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
